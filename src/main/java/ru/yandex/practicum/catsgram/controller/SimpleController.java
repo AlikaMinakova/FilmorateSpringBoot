94b1b2e7b1dc93e8a611ebc4992b5d05de4594ce
AlikaMinakova <minackowa.ali@yandex.ru>
2024-10-03 13:42:56 +0300

    add Post object

--- src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/SimpleController.java
@@ -1,15 +1,18 @@
 package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
-//класс для создания эндпоинтов(обработчики адреса)
-@RestController //обработчик пониает что класс содержит обработчики адресов
+@RestController //  обработчик понимает что класс содержит обработчики адресов
+//  @RequestMapping(value = "/home/corporation/") - попадаем в этот класс по адресу
 public class SimpleController {
 
-    @RequestMapping("/home")
+    //    @RequestMapping(value = "/home", method = RequestMethod.GET)
+    @GetMapping("/home")
     public String homePage() {
         return "Котограм";
     }
